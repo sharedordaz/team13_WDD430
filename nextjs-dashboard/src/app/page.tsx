35277b2e9ab405b5628980d94077f51404806bf9
sharedordaz <sharedordaz@live.com.mx>
2024-01-22 18:59:19 -0600

    Assigned work

--- nextjs-dashboard/src/app/page.tsx
+++ nextjs-dashboard/src/app/page.tsx
@@ -4,6 +4,21 @@ import NavBar from "@/app/nav";
 import Banner from '@/app/banner'
 import Logo from '@/app/logo'
 
+/*
+TODO:
+-Shopping Cart (Douglas Fabricio)
+-Main Grid (Shared Ordaz)
+-Header (Camila Apai)
+-Footer (Douglas Fabricio)
+
+Ideas:
+https://www.coolmaison.com/
+https://www.globant.com/about
+
+*/
+
+
+
 export default function Home() {
   return (
     <main className={styles.main}>
