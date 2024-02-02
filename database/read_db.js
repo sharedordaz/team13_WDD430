566fbd5edd4ed590a73ad6c0c81d20d19ce00e30
sharedordaz <sharedordaz@live.com.mx>
2024-02-02 00:34:53 -0600

    Now with a completely functional MongoDB API

--- database/read_db.js
+++ database/read_db.js
@@ -1,12 +1,19 @@
 const { client } = require('./connect.js')
 
-function databasetoJSON(req, res){
+async function databasetoJSON(req, res){
+     try{
      const database = client.db('Handcraft_Haven');
      const collection = database.collection('artworks');
      
-     const data =  collection.find({}).toArray();
+     const data =  await collection.find({}).toArray();
+     console.log(data);
      res.writeHead(200, { 'Content-Type': 'application/json' });
      res.end(JSON.stringify(data));
+    }catch (error){
+     console.error("Error fetching data from the database");
+     res.writeHead(500, { 'Content-Type': 'text/plain' });
+     res.end('Internal Server Error');
+    }
     
 }
 
