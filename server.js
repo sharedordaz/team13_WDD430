7848f6fc7eb592aab119f71caf3a405c6c100c8b
sharedordaz <sharedordaz@live.com.mx>
2024-02-15 23:01:39 -0600

    Now auto-compilintg

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const next = require('next');
 const { connectToDatabase } = require('./database/connect.js');
 const { databasetoJSON } = require('./database/read_db.js');
 
+const { exec } = require('child_process');
+const http = require('http');
+
 //const dev = process.env.NODE_ENaV;
 const dev = process.env.NODE_ENV !== 'production';
 console.log("DEV:", dev);
@@ -26,11 +29,18 @@ app.prepare().then(() => {
   server.get('*', async (req, res) => {
     return handle(req, res);
   })
+
+
   server.listen(port, async (err) => {
     if (err) throw err;
     try{
         await connectToDatabase();
         console.log('\x1b[1m','\x1b[32m', 'Connected to the database', '\x1b[0m')
+
+        const url = 'http://localhost:' + port
+
+        console.log(url);
+        exec('curl')
             }
     catch(error){
         console.error('Error: ', error);
@@ -38,5 +48,26 @@ app.prepare().then(() => {
     }
    
     console.log('\x1b[36m', `> Ready on Port`, '\x1b[1m', '\x1b[31m', `${port}`, '\x1b[0m');
+    const options = {
+        hostname: 'localhost', // Assuming your server is running on localhost
+        port: 3000, // Assuming your server is running on port 3000, change accordingly
+        path: '/?sort=alphre', // The endpoint you want to hit
+        method: 'GET' // or 'POST', 'PUT', 'DELETE', etc. depending on your needs
+    };
+    const req = http.request(options, (res) => {
+    console.log(`statusCode: ${res.statusCode}`);
+
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+    });
+
+    req.on('error', (error) => {
+    console.error(error);
+    });
+
+    req.end();
+
+
   });
 });
