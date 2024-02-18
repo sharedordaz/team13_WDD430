4aab682492f807f62533a0a35a81a3c65fbb42dd
sharedordaz <sharedordaz@live.com.mx>
2024-02-17 21:42:40 -0600

    CSS RESPONSIBLE DONE

--- src/app/nav.tsx
+++ src/app/nav.tsx
@@ -4,9 +4,7 @@ import Link from 'next/link';
 export default function NavBar() {
   return (
   <nav className={styles.nav}>
-      <ul>
-        <li><a href="/contact">&#9993; Contact Us</a></li>
-    </ul>
+    <a href="/contact">&#9993; Contact Us</a>
     <div className={styles.shoppingCart}>
           <Link href="/cart">
               <img src="cart.png" alt="Carrito de compras" />
